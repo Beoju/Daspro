388318d39a6725682dfd2e6fceb15e8f4b111767
Adyuta Raksa Ramadhana <95728938+Beoju@users.noreply.github.com>
2023-10-11 13:10:12 +0700

    Add files via upload

---
+++ PemilihanPercobaan202.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class PemilihanPercobaan202{
+    public static void main(String[] args) {
+        Scanner input02 = new Scanner (System.in);
+        System.out.println("Nilai uas: ");
+        float uas = input02.nextFloat();
+        System.out.println("Nilai uts: ");
+        float uts = input02.nextFloat();
+        System.out.println("Nilai kuis ");
+        float kuis = input02.nextFloat();
+        System.out.println("Nilai tugas: ");
+        float tugas = input02.nextFloat();
+
+        float total = (uas * 0.4F) + (uts * 0.3F) + (kuis * 0.1F) + (tugas * 0.2F);
+        String message = total < 65 ? "Remidi" : "Tidak Remidi";
+        System.out.println("Nilai akhir = " + total + " sehingga " + message);
+    }
+}

a8c96cb9e10cb58d03474d8f608c05ef4b852052
Adyuta Raksa Ramadhana <95728938+Beoju@users.noreply.github.com>
2023-10-11 12:53:40 +0700

    Update PemilihanPercobaan102.java

--- PemilihanPercobaan102.java
+++ PemilihanPercobaan102.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class PemilihanPercobaan202{
+public class PemilihanPercobaan102{
     public static void main(String[] args) {
         Scanner input02 = new Scanner (System.in);
          System.out.println("Masukan angka: ");

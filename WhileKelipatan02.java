7859cbfecbbe6e5a5fd85068b4f5c7e5c9eb69b6
Adyuta Raksa Ramadhana <95728938+Beoju@users.noreply.github.com>
2023-11-07 19:03:05 +0700

    Update WhileKelipatan02.java

--- WhileKelipatan02.java
+++ WhileKelipatan02.java
@@ -1,24 +1,24 @@
 import java.util.Scanner;
 
 public class WhileKelipatan02 {
-    
+
     public static void main(String[] args) {
-        Scanner sc = new Scanner (System.in);
-        
-        int n, i = 1;
-        float jml = 0 ,x, rata;
-        System.out.println("Banyaknya Data: ");
-        n = sc.nextInt();
+        Scanner scan02 = new Scanner(System.in);
 
-        while (i <= n) {
-            System.out.print("Data ke-"+i+": ");
-            x = sc.nextFloat();
-            jml += x;
-            i++;
+        int kelipatan = 0, jumlah = 0, counter = 0;
+        System.out.println("Masukkan bilangan kelipatan (1-9): ");
+        kelipatan = scan02.nextInt();
+
+        int i = 1; // Inisialisasi nilai i di luar loop
+        while (i <= 50) { // Ganti loop for dengan while
+            if (i % kelipatan == 0) {
+                jumlah += i;
+                counter++;
+            }
+            i++; // Perbarui nilai i setiap iterasi
+            double rataRata = (double) jumlah / counter; // Hitung rata-rata
         }
-        rata = jml / n;
-        System.out.println("Rata - rata: " + rata);
-        System.out.println("Jumlah: " + jml);
-
+        System.out.printf("Banyaknya %d dari 1 sampai 50 adalah %d\n", kelipatan, counter);
+        System.out.printf("Total bilangan kelipatan %d dari 1 sampai 50 adalah %d\n", kelipatan, jumlah);
     }
 }
